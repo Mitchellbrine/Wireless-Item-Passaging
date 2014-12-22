66da9bcd52f7b93c8be5fd8bc8b2b5a446954b54
Mitchellbrine <awsomeman500@gmail.com>
2014-12-21 22:37:17 -0800

    Flipped things to make them correct

--- src/main/java/mc/Mitchellbrine/wip/util/ConfigUtils.java
+++ src/main/java/mc/Mitchellbrine/wip/util/ConfigUtils.java
@@ -16,7 +16,7 @@ public class ConfigUtils {
 
         range = config.get("conduit","Conduit GPS Range",50,"Sets the max range for the GPS to set the coordinates for the Conduit").getInt(50);
         delay = config.get("conduit","Conduit Transport Delay",5,"Sets the delay of the conduit based on tick rate (20 = 1 second)").getInt(5);
-        dBlocksRequired = config.getInt("recipe","Diamond Blocks Required",8,1,8,"Sets the amount of diamond blocks in the Wither-Infused Diamond Blocks");
+        dBlocksRequired = config.getInt("Diamond Blocks Required", "recipe", 8, 1, 8, "Sets the amount of diamond blocks in the Wither-Infused Diamond Blocks");
 
         config.save();
     }
